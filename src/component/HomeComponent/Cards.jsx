2536cfd5017bde8601b6d938a54c94edddf6344d
Ibrahim <ibrahimhosen4045@gmail.com>
2026-04-16 01:43:44 +0600

    add route this project

--- src/component/HomeComponent/Cards.jsx
+++ src/component/HomeComponent/Cards.jsx
@@ -6,8 +6,8 @@ const Cards = () => {
   const {friend, loading} = UseHooks()
   return (
     <div className=' container w-11/12 mx-auto py-10'>
-      <h1 className='text-2xl font-semibold'>Your Friends</h1>
-      <div>
+      <h1 className='text-2xl font-semibold py-3'>Your Friends</h1>
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6'>
         {
           friend.map((info, index) => 
               <DesignCard key={index} info = {info}></DesignCard>
--- src/component/HomeComponent/ui/DesignCard.jsx
+++ src/component/HomeComponent/ui/DesignCard.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 const DesignCard = ({info}) => {
   return (
     <div>
-      <div>
+      <div className='border border-gray-200 shadow-sm rounded-lg flex flex-col items-center justify-center p-6 gap-2.5 bg-white'>
+        <img src={info.picture} alt="" />
         <h1 className='font-semibold text-xl'>{info.name}</h1>
         <p className='text-[12px] text-[#64748B]'>62d ago</p>
-        <div className='flex gap-3'>
+        <div className='flex gap-2 justify-center'>
             {info.tags.map(tag => <div>
                 <button className=' font-medium text-[12px] py-2 px-3 bg-[#CBFADB] rounded-full text-[#244D3F]'>{tag}</button>
             </div>)}
         </div>
-        <button className={({bg}) => `font-medium text-[14px]  py-1.75 px-3 rounded-full text-[#244D3F] ${bg === "overdue" ? "bg-red-500" : ""}`}>{info.status}</button>
+        <button className={`font-medium text-[14px]  py-1.75 px-3 rounded-full text-white capitalize ${info.status === "overdue" ? "bg-[#EF4444]" : info.status === "on-track" ? "bg-[#244D3F]" : "bg-[#EFAD44]"}`}>{info.status}</button>
       </div>
     </div>
   )
